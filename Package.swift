86230a170887e3b8588aa5b0505de30e9bfe8f0f
tealshift <tealshift@gmail.com>
2021-08-16 16:54:06 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "CocoaLumberjack",
     platforms: [
-        .iOS(.v9),
+        .iOS(.v13),
         .macOS(.v10_10),
         .watchOS(.v3),
         .tvOS(.v9),
